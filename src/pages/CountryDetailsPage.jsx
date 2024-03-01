2c6dabda693b51705fb7250434c60cbf080e53da
Omayma El Hadari <om.elhadari@gmail.com>
2024-03-02 00:10:18 +0100

    solved lab

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,74 @@
-function CountryDetails() {}
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
+import axios from "axios";
+
+function CountryDetails() { 
+    const [fetching, setFetching] = useState(true);
+    const [countries, setCountries] = useState(null);
+    const { countryId } = useParams();
+    const apiURL = "https://ih-countries-api.herokuapp.com/countries";
+
+    useEffect(() => {
+        axios.get(apiURL + `/${countryId}`).then((fetch) => {
+            setCountries(fetch.data);
+            setFetching(false)
+        }).catch((err) => console.log(err))
+    }, [countryId]);
+
+    return (
+        <div className="container">
+          <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+    
+          {fetching && <p>Loading...</p>}
+    
+          {!countries && <p>Country not found!</p>}
+    
+          {!fetching && countries && (
+            <div>
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${countries.alpha2Code.toLowerCase()}.png`}
+                alt="country flag"
+              />
+              <h1>{countries.name.common}</h1>
+              <br />
+              <table className="table">
+                <thead></thead>
+                <tbody>
+                  <tr>
+                    <td style={{ width: "30%" }}>Capital</td>
+                    <td>{countries.capital}</td>
+                  </tr>
+                  <tr>
+                    <td>Area</td>
+                    <td>
+                      {countries.area} km
+                      <sup>2</sup>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Borders</td>
+                    <td>
+                      <ul>
+                        {countries.borders.map((border) => {
+                          return (
+                            <li key={border}>
+                              <Link key={border} to={`/${border}`}>
+                                {border}
+                              </Link>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      );
+    
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,58 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [countries, setCountries] = useState([]);
+  //   //   const [fetching, setFetching] = useState(true);
+  const apiURL = "https://ih-countries-api.herokuapp.com/countries";
+
+  useEffect(() => {
+    axios.get(apiURL).then((fetch) => {
+      setCountries(fetch.data);
+      console.log("data", fetch.data);
+      //         // setFetching(false);
+    });
+  }, []);
+
+  return (
+    <div
+      className="container"
+      style={{ maxHeight: "90vh", overflow: "scroll" }}
+    >
+      <h1 style={{ fontSize: "24px" }}>
+        WikiCountries: Your Guide to the World
+      </h1>
+
+      {/* {fetching && (
+//         <img
+//           src={
+//             "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/m3/react-lists/spinner.gif"
+//           }
+//         />
+//       )} */}
+      {!countries && <p>Countries not found!</p>}
+
+      {countries && (
+        <div className="list-group">
+          {countries.map((country) => (
+            <Link
+              key={country._id}
+              className="list-group-item list-group-item-action"
+              to={`/${country.alpha3Code}`}
+            >
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt="country flag"
+              />
+              <br />
+              {country.name.common}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default HomePage;
